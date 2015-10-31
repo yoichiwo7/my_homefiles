2eca491040f8048611ae52be90079753e083b992
Yoichi Iwaki <yoichiwo7@gmail.com>
2015-10-31 17:51:45 +0900

    Added emacs setting.

---
+++ .emacs.d/init.el
@@ -0,0 +1,94 @@
+;;------------------------------------------------------------------------------
+;; General Settings
+;;------------------------------------------------------------------------------
+
+; hide menu-bar and tool-bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+; show line number
+(global-linum-mode t)
+(setq linum-format "%4d  ")
+
+; highlight matched parences
+(show-paren-mode 1)
+;(setq show-paren-style 'mixed)
+
+; highlight current line
+;(global-hl-line-mode)
+
+; no backups, remove autosaves
+(setq backup-inhibited t)
+(setq delete-auto-save-files t)
+
+; save cursor
+(require 'saveplace)
+(setq-default save-place t)
+
+
+;;------------------------------------------------------------------------------
+;; Japanese Settings
+;;------------------------------------------------------------------------------
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
+
+
+;;------------------------------------------------------------------------------
+;; Package Settings
+;;------------------------------------------------------------------------------
+(require 'package)
+
+;; add package URLS
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+; no auto startup for packages
+(setq package-enable-at-startup nil)
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it's not.
+  Return a list of installed packages or nil for every skipped package."
+  (mapcar
+    (lambda (package)
+      (if (package-installed-p package)
+        nil
+        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+          (package-install package)
+          package)))
+    packages))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+;; Activate installed packages
+(package-initialize)
+
+;; Check and install packages
+(ensure-package-installed 
+    'auto-complete
+    'helm
+    'evil
+    'flycheck
+    ;'magit
+    'org
+    'projectile
+    'solarized-theme
+    'undo-tree
+    'yasnippet
+    )
+
+
+;;------------------------------------------------------------------------------
+;; Color Settings
+;;------------------------------------------------------------------------------
+(load-theme 'wombat t)
+
+
+;;------------------------------------------------------------------------------
+;; Vim Settings (evilmode)
+;;------------------------------------------------------------------------------
+(require 'evil)
+(evil-mode t)
+
